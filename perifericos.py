e777b5546e311cc7985b6a507b34617e9cbe8b27
Kioshi Kiyan <kioshi_kiyan@outlook.com>
2023-01-31 18:02:27 -0500

    Funciones de cámara añadidas a periféricos

--- perifericos.py
+++ perifericos.py
@@ -1,3 +1,15 @@
+import cv2
 # Cámara
+class Camara:
+    cap = None
+
+    def __init__(self):
+        if self.__class__.cap is None:
+            self.__class__.cap = cv2.VideoCapture(0)
+        self.camara = self.__class__.cap
+
+    def read(self):
+        _, frame = self.camara.read()
+        cv2.imshow('Frame', frame)
 
 # Arduino
