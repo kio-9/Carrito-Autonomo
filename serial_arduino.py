cf3b01e83e56ca40df3c714d0f8c9066221f708d
Cristhian Vasquez <cristhianvasquezjimenez@gmail.com>
2023-02-02 10:58:53 -0500

    Prueba de conexión completa

--- serial_arduino.py
+++ serial_arduino.py
@@ -1,4 +1,4 @@
-# sudo chmod 666 /dev/ttyACM0
+# sudo chmod 666 /dev/ttyUSB1
 import serial
 
 class Arduino:
@@ -6,12 +6,13 @@ class Arduino:
 
     def __init__(self):
         if self.__class__.ser is None:
-            self.__class__.ser = serial.Serial('/dev/ttyACM0')
+            self.__class__.ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
         self.ser = self.__class__.ser
 
     def sendCommand(self, command):
         command = command+"\n"
         self.ser.write(command.encode('utf_8'))
+        print(self.ser.readline().decode())
         print(self.ser.readline().decode())
         
 if __name__ == '__main__':
